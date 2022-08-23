e9fdec3cb441da979a4b5329efc32c26dd491517
sdelsjouGitHub <41159201+sdelsjouGitHub@users.noreply.github.com>
2022-08-23 16:42:07 +0200

    Add files via upload

---
+++ VOD.Admin/Pages/Courses/Edit.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using VOD.Common.DTOModels.Admin;
+using VOD.Common.Entities;
+using VOD.Common.Extensions;
+using VOD.Common.Services;
+
+namespace VOD.Admin.Pages.Courses
+{
+    [Authorize(Roles = "Admin")]
+    public class EditModel : PageModel
+    {
+        #region Properties
+        private readonly IAdminService _db;
+        [BindProperty] public CourseDTO Input { get; set; } = new CourseDTO();
+        [TempData] public string Alert { get; set; }
+        #endregion
+
+        #region Constructor
+        public EditModel(IAdminService db)
+        {
+            _db = db;
+        }
+        #endregion
+
+        #region Actions
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            try
+            {
+                Alert = string.Empty;
+                ViewData["Instructors"] = (await _db.GetAsync<Instructor, InstructorDTO>()).ToSelectList("Id", "Name");
+                Input = await _db.SingleAsync<Course, CourseDTO>(s => s.Id.Equals(id));
+                return Page();
+            }
+            catch
+            {
+                return RedirectToPage("/Index", new { alert = "You do not have access to this page." }); ;
+            }
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var succeeded = await _db.UpdateAsync<CourseDTO, Course>(Input);
+                if (succeeded)
+                {
+                    // Message sent back to the Index Razor Page.
+                    Alert = $"Updated Course: {Input.Title}.";
+                    return RedirectToPage("Index");
+                }
+            }
+
+            // Reload the modules when the page is reloaded
+            ViewData["Instructors"] = (await _db.GetAsync<Instructor, InstructorDTO>()).ToSelectList("Id", "Name");
+            // Something failed, redisplay the form.
+            return Page();
+        }
+        #endregion
+    }
+}
---
+++ VOD.Admin/Pages/Instructors/Index.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using VOD.Common.DTOModels.Admin;
+using VOD.Common.Entities;
+using VOD.Common.Services;
+
+namespace VOD.Admin.Pages.Instructors
+{
+    [Authorize(Roles = "Admin")]
+    public class IndexModel : PageModel
+    {
+        #region Properties
+        private readonly IAdminService _db;
+        public IEnumerable<InstructorDTO> Items = new List<InstructorDTO>();
+        [TempData] public string Alert { get; set; }
+
+        #endregion
+
+        #region Constructor
+        public IndexModel(IAdminService db)
+        {
+            _db = db;
+        }
+        #endregion
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            try
+            {
+                Items = await _db.GetAsync<Instructor, InstructorDTO>(true);
+                return Page();
+            }
+            catch
+            {
+                Alert = "You do not have access to this page.";
+                return RedirectToPage("/Index");
+            }
+        }
+
+    }
+}
